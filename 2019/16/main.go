705b78074d01a01792fe4a77277ac2a354410568
Brian Beck <brian.beck@outlook.com>
2019-12-15 21:37:48 -0800

    2019 Day 16

---
+++ 2019/16/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+
+	"../aoc"
+)
+
+func main() {
+	input := aoc.ReadFileLines("input.txt")
+	sw := aoc.NewStopwatch()
+	// Part 1
+	digits := readDigits(input[0])
+	fmt.Println(readDigitSlice(fft(digits, 100)[:8]))
+	fmt.Println(sw.Elapsed())
+
+	// Part 2
+	sw.Reset()
+	fmt.Println(part2(digits))
+	fmt.Println(sw.Elapsed())
+}
+
+func readDigits(input string) []int64 {
+	digits := []int64{}
+	for _, r := range []rune(input) {
+		digits = append(digits, int64(r-'0'))
+	}
+	return digits
+}
+
+func fft(digits []int64, rounds int) []int64 {
+	result := make([]int64, len(digits))
+	copy(result, digits)
+	for round := 0; round < rounds; round++ {
+		result = fftRound(result)
+	}
+	return result
+}
+
+func fftRound(digits []int64) []int64 {
+	result := make([]int64, len(digits))
+
+	for i := 0; i < len(digits); i++ {
+		p := buildPattern(i+1, len(digits))
+		sum := int64(0)
+		for j := 0; j < len(digits); j++ {
+			sum += digits[j] * p[j]
+		}
+		result[i] = aoc.Abs(sum % 10)
+	}
+
+	return result
+}
+
+var basePattern = []int64{0, 1, 0, -1}
+
+func buildPattern(pos, length int) []int64 {
+	pattern := make([]int64, length+1) // We need to drop the first character, so add one more on the end
+
+	for i := 0; i < len(pattern); i++ {
+		pattern[i] = basePattern[(i/pos)%len(basePattern)]
+	}
+
+	return pattern[1:]
+}
+
+func part2(baseDigits []int64) int {
+	// Build the full sequence
+	digits := []int64{}
+	for i := 0; i < 10000; i++ {
+		digits = append(digits, baseDigits...)
+	}
+	// Keep only the bits starting at our offset
+	answerIndex := readDigitSlice(digits[:7])
+	digits = digits[answerIndex:]
+
+	// Looking at the last few digits of each iteration, for the later digits of the sequence, the
+	// formula each iteration is:
+	// d[i] = sum(d[i:]) % 10
+	for round := 0; round < 100; round++ {
+		sum := int64(0)
+		for i := len(digits) - 1; i >= 0; i-- {
+			sum += digits[i]
+			digits[i] = aoc.Abs(sum) % 10
+		}
+	}
+
+	return readDigitSlice(digits[:8])
+}
+
+func readDigitSlice(digits []int64) int {
+	answer := 0
+	for _, d := range digits {
+		answer *= 10
+		answer += int(d)
+	}
+	return answer
+}
---
+++ 2019/16/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+
+	"../aoc"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Examples of program input and output from https://adventofcode.com/2019/day/16
+func TestReadDigits(t *testing.T) {
+	assert.Equal(t, []int64{1, 4, 2, 3}, readDigits("1423"))
+}
+
+func TestModulo(t *testing.T) {
+	assert.Equal(t, 7, 37%10)
+	assert.Equal(t, int64(7), aoc.Abs(-17%10))
+}
+
+func TestPattern(t *testing.T) {
+	assert.Equal(t, []int64{0, 1, 1, 0, 0, -1, -1, 0, 0, 1, 1, 0, 0, -1}, buildPattern(2, 14))
+}
+
+func TestFft(t *testing.T) {
+	assert.Equal(t, []int64{4, 8, 2, 2, 6, 1, 5, 8}, fft([]int64{1, 2, 3, 4, 5, 6, 7, 8}, 1))
+	assert.Equal(t, []int64{3, 4, 0, 4, 0, 4, 3, 8}, fft([]int64{1, 2, 3, 4, 5, 6, 7, 8}, 2))
+	assert.Equal(t, []int64{0, 3, 4, 1, 5, 5, 1, 8}, fft([]int64{1, 2, 3, 4, 5, 6, 7, 8}, 3))
+	assert.Equal(t, []int64{0, 1, 0, 2, 9, 4, 9, 8}, fft([]int64{1, 2, 3, 4, 5, 6, 7, 8}, 4))
+}
+
+func TestFftExtended(t *testing.T) {
+	assert.Equal(t, []int64{2, 4, 1, 7, 6, 1, 7, 6}, fft(readDigits("80871224585914546619083218645595"), 100)[:8])
+	assert.Equal(t, []int64{7, 3, 7, 4, 5, 4, 1, 8}, fft(readDigits("19617804207202209144916044189917"), 100)[:8])
+	assert.Equal(t, []int64{5, 2, 4, 3, 2, 1, 3, 3}, fft(readDigits("69317163492948606335995924319873"), 100)[:8])
+}
+
+func TestReadDigitSlice(t *testing.T) {
+	assert.Equal(t, 1234, readDigitSlice([]int64{1, 2, 3, 4}))
+}
+
+func TestPart2(t *testing.T) {
+	assert.Equal(t, 84462026, part2(readDigits("03036732577212944063491565474664")))
+}
