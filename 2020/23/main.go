80fc8286a174317fe3e7a06cecf2129329d37e13
Brian Beck <brian.beck@outlook.com>
2020-12-22 22:32:29 -0800

    2020 Day 23B

--- 2020/23/main.go
+++ 2020/23/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"../../aoc"
 )
 
 // https://adventofcode.com/2020/day/23
 // A cup shuffle game
-
-// Enable debug tracing
-const debug = false
-const debugIt = true
 
 func main() {
 	sw := aoc.NewStopwatch()
@@ -22,134 +17,116 @@ func main() {
 
 	sw.Reset()
 	fmt.Println("Step 2:")
-	fmt.Println(step2(364297581))
+	_, _, s2 := step2(364297581)
+	fmt.Println(s2)
 	fmt.Println(sw.Elapsed())
 }
 
 func step1(input, iterations int) int {
-	c := newCups(input)
+	digits := splitDigits(input)
+	c := newCups(digits)
 	for i := 0; i < iterations; i++ {
-		if debug {
-			fmt.Printf("-- move %d --\n", i+1)
-		}
 		c.move()
 	}
 	return c.value()
 }
 
 func step2(input int) (int64, int64, int64) {
-	c := newCupsPadded(input, 1000000)
-	for i := 0; i < 10000000; i++ {
-		if debug {
-			fmt.Printf("-- move %d --\n", i+1)
-		}
+	dimension := 1000000
+	iterations := 10000000
+	digits := splitDigits(input)
+	padded := make([]int, dimension)
+	copy(padded, digits)
+	for i := len(digits); i < dimension; i++ {
+		padded[i] = i + 1
+	}
+	c := newCups(padded)
+	for i := 0; i < iterations; i++ {
 		c.move()
-		if debugIt && i%10000 == 0 {
-			fmt.Printf("-- move %d --\n", i)
-		}
 	}
 
-	one := 0
-	for ; c.c[one] != one; one++ {
-	}
-	first := int64(c.c[(one+1)%len(c.c)])
-	second := int64(c.c[(one+2)%len(c.c)])
+	one := c.nodes[1]
+	first := int64(one.next.val)
+	second := int64(one.next.next.val)
 	return first, second, first * second
 }
 
+type node struct {
+	val  int
+	next *node
+}
+
+func newNode(val int) *node {
+	n := node{val: val, next: nil}
+	return &n
+}
+
 type cups struct {
-	c       []int
-	current int
+	head    *node
+	current *node
+	nodes   map[int]*node
 	l       int // Length of the cups array.  Also the max number
 }
 
-func newCups(input int) *cups {
-	var c []int
+func splitDigits(input int) []int {
+	var val []int
 	for input > 0 {
-		c = append([]int{input % 10}, c...)
+		val = append([]int{input % 10}, val...)
 		input /= 10
 	}
-	cu := cups{c: c, current: 0, l: len(c)}
-	return &cu
+	return val
 }
 
-func newCupsPadded(input int, dimension int) *cups {
-	tmp := newCups(input)
-	c := make([]int, dimension)
-	for i, v := range tmp.c {
-		c[i] = v
-	}
-	for i := len(tmp.c); i < dimension; i++ {
-		c[i] = i + 1
-	}
-	cu := cups{c: c, current: 0, l: dimension}
-	return &cu
-}
+func newCups(input []int) *cups {
+	// Create the first node
+	val := input[0]
+	prev := newNode(val)
+	c := cups{
+		head:    prev,
+		current: prev,
+		nodes:   make(map[int]*node),
+		l:       len(input)}
+	c.nodes[val] = prev
 
-func (c *cups) String() string {
-	var b strings.Builder
-	for i, v := range c.c {
-		if i == c.current {
-			fmt.Fprintf(&b, "(%d) ", v)
-
-		} else {
-			fmt.Fprintf(&b, "%d ", v)
-		}
+	for i := 1; i < c.l; i++ {
+		val = input[i]
+		curr := newNode(val)
+		prev.next = curr
+		c.nodes[val] = curr
+		prev = curr
 	}
-	return b.String()
+	// Complete the circle
+	c.nodes[input[c.l-1]].next = c.head
+
+	return &c
 }
 
 func (c *cups) value() int {
 	v := 0
-	one := 0
-	for c.c[one] != 1 {
-		one++
+
+	one := c.nodes[1]
+	for curr := one.next; curr != one; curr = curr.next {
+		v *= 10
+		v += curr.val
 	}
 
-	for i := one + 1; i < c.l; i++ {
-		v *= 10
-		v += c.c[i]
-	}
-
-	for i := 0; i < one; i++ {
-		v *= 10
-		v += c.c[i]
-	}
 	return v
 }
 
 func (c *cups) move() {
-	if debug {
-		fmt.Printf("cups: %s\n", c)
-	}
-	result := make([]int, len(c.c))
-	pickup := make([]int, 3)
+	pickupHead := c.current.next
+	pickupTail := pickupHead.next.next
+	c.current.next = pickupTail.next
 
-	result[c.current] = c.c[c.current]
-	cIt := (c.current + 1) % c.l
-	resultIt := cIt
-	for i := 0; i < 3; i++ {
-		pickup[i] = c.c[cIt]
-		cIt = (cIt + 1) % c.l
-	}
-	if debug {
-		fmt.Print("pick up: ")
-		for i, v := range pickup {
-			fmt.Print(v)
-			if i < 2 {
-				fmt.Print(", ")
-			}
-		}
-		fmt.Println("")
-	}
+	puVal := [3]int{pickupHead.val, pickupHead.next.val, pickupTail.val}
 
-	dest := c.c[c.current] - 1
+	dest := c.current.val - 1
 	if dest < 1 {
 		dest += c.l
 	}
 	for foundDest := true; foundDest; {
 		foundDest = false
-		for _, v := range pickup {
+		for _, v := range puVal {
 			if v == dest {
 				foundDest = true
 				break
@@ -162,32 +139,12 @@ func (c *cups) move() {
 			}
 		}
 	}
-	if debug {
-		fmt.Printf("destination: %d\n\n", dest)
-	}
 
-	// Copy values until we get to the destination
-	for foundDest := false; !foundDest; {
-		result[resultIt] = c.c[cIt]
-		foundDest = dest == c.c[cIt]
-		cIt = (cIt + 1) % c.l
-		resultIt = (resultIt + 1) % c.l
-	}
-
-	// Copy the picked up values
-	for i := 0; i < 3; i++ {
-		result[resultIt] = pickup[i]
-		resultIt = (resultIt + 1) % c.l
-	}
-
-	// Copy any remaining falues
-	for resultIt != c.current {
-		result[resultIt] = c.c[cIt]
-		cIt = (cIt + 1) % c.l
-		resultIt = (resultIt + 1) % c.l
-	}
+	// Insert the pickup after the destination
+	d := c.nodes[dest]
+	pickupTail.next = d.next
+	d.next = pickupHead
 
 	// Move the current node
-	c.c = result
-	c.current = (c.current + 1) % c.l
+	c.current = c.current.next
 }
--- 2020/23/main_test.go
+++ 2020/23/main_test.go
@@ -12,5 +12,8 @@ func TestStep1(t *testing.T) {
 }
 
 func TestStep2(t *testing.T) {
-	assert.Equal(t, -1, step2(389125467))
+	first, second, product := step2(389125467)
+	assert.Equal(t, int64(934001), first)
+	assert.Equal(t, int64(159792), second)
+	assert.Equal(t, int64(149245887792), product)
 }
