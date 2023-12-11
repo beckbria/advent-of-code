0a181ccc9bb48c80173169ab1b7e230746bf0f22
Brian Beck <brian.beck@outlook.com>
2023-12-11 10:00:57 -0800

    Scaffolding for 2023/03-11

---
+++ 2023/04/main.kt
@@ -0,0 +1,28 @@
+package beckbria.aoc2023.day04
+
+import beckbria.aoc2023.util.readInput
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
+
+fun bothParts(testInput: List<String>) {
+    println("Part 1:")
+    println(part1(testInput))
+    println("\nPart 2:")
+    println(part2(testInput))
+}
+
+fun main() {
+    val testInputExample = readInput("2023/04/input_example.txt")
+    val testInput = readInput("2023/04/input.txt")
+    println("Example Input:")
+    bothParts(testInputExample)
+    println("\nYour Input:")
+    bothParts(testInput)
+}
+
